ac1a8e2eadebaa1a9cf7b687f397324094ea4a9d
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-05 09:25:16 -0300

    detalhes cartao

--- public/client.js
+++ public/client.js
@@ -1,3 +1,6 @@
+var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
@@ -11,4 +14,30 @@ TrelloPowerUp.initialize({
       }
     }];
   },
+  'card-badges': function(t, options) {
+    return t.get('card', 'shared', 'estimate')
+      .then(function(estimate) {
+        return [{
+          icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+          text: estimate || 'No Estimate!',
+          color: estimate ? null : 'red',
+        }];  
+      });
+  },
+  'card-detail-badges': function(t, options) {
+    return t.get('card', 'shared', 'estimate')
+    .then(function(estimate) {
+      return [{
+        title: 'Estimate',
+        text: estimate || 'No Estimate!',
+        color: estimate ? null : 'red',
+        callback: function(t) {
+          return t.popup({
+            title: "Pontuação",
+            url: 'pontuation.html',
+          });
+        }
+      }]
+    });
+  }
 });
