09f7555807120427f9e0930cd73892e711b3d420
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-07 10:41:07 -0300

    pagination input 15

--- public/components/pagination-input.js
+++ public/components/pagination-input.js
@@ -1,11 +1,11 @@
 function preparePaginationInputItem(item, active) {
   var activeClass = active === item ? 'active' : '';
-  return '<li class="page-item ' + activeClass + '"><a class="page-link" href="#">' + item + '</a></li>'; 
+  return '<li class="page-item value-item ' + activeClass + '"><a class="page-link" href="#">' + item + '</a></li>'; 
 }
 
 function calculatePaginationOffset(data, active, maxItens) {
   var sideSize = Math.floor(maxItens/2);
-  var activeIndex = R.findIndex(R.equals(active))(data);
+  var activeIndex = paginationInputActiveIndex(data, active);
   var max = data.length;
   var negativeItemOffset = max - activeIndex;
   var relativeItemOffset = activeIndex - sideSize + 1;
@@ -19,26 +19,69 @@ function calculatePaginationOffset(data, active, maxItens) {
   return relativeItemOffset;
 }
 
-function paginationInputItemSelect() {
+function paginationInputActiveIndex(data, active) {
+  return R.findIndex(R.equals(active))(data);
+}
 
+function paginationInputIsFirst(data, active) {
+  var activeIndex = paginationInputActiveIndex(data, active);
+  if (activeIndex <= 0 ) {
+    return;
+  }
+} 
+
+function paginationInputIsLast(data, active) {
+  var activeIndex = paginationInputActiveIndex(data, active);
+  return activeIndex >= data.length; 
+} 
+
+function paginationInputItemSelect() {
+}
+
+function paginationInputPreviousPage(data, container, active) {
+  if (paginationInputIsFirst(data, active)) {
+    return;
+  }
+  var activeIndex = paginationInputActiveIndex(data, active);
+  renderPaginationInput(data, container, data[activeIndex - 1])
+}
+
+function paginationInputNextPage(data, container, active) {
+  if (paginationInputIsLast(data, active)) {
+    return;
+  }
+  var activeIndex = paginationInputActiveIndex(data, active);
+  renderPaginationInput(data, container, data[activeIndex - 1])
 }
 
 function renderPaginationInput(data, container, active) {
   var maxItens = 7;
-  var offset = calculatePaginationOffset(data, active, 7);
-  applyToElements(container.querySelectorAll('nav>ul.pagination>li'), function(elm) {
-    elm.removeEventListener('click', paginationInputItemSelect, false);
+  var offset = calculatePaginationOffset(data, active, maxItens);
+  applyToElements(container.querySelectorAll('.page-item.value-item'), function(elm) {
+    elm.removeEventListener('click', function() {paginationInputItemSelect(data, container, active);}, false);
+  });
+  applyToElements(container.querySelectorAll('.page-item:first-child'), function(elm) {
+    elm.removeEventListener('click',function() {paginationInputPreviousPage(data, container, active);}, false);
+  });
+  applyToElements(container.querySelectorAll('.page-item:last-child'), function(elm) {
+    elm.removeEventListener('click',function() {paginationInputNextPage(data, container, active);}, false);
   });
   container.innerHTML = '';
   var component = "";
-  component = component +  '<nav> <ul class="pagination"> <li class="page-item"><a class="page-link" href="#"><i class="fas fa-chevron-left"></i></a></li>'
+  component = component +  '<nav> <ul class="pagination"> <li class="page-item ' + paginationInputIsFirst(data, active) ? 'disabled' : '' + '"><a class="page-link" href="#"><i class="fas fa-chevron-left"></i></a></li>'
   for (var i = offset; i < (maxItens + offset) && i < data.length; i++) {
     component = component + preparePaginationInputItem(data[i], active) + '\n';
   }
-  component = component +  '<li class="page-item"><a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a></li></ul></nav>'
+  component = component +  '<li class="page-item ' + paginationInputIsLast(data, active) ? 'disabled' : '' + '"><a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a></li></ul></nav>'
   container.innerHTML = component;
-  applyToElements(container.querySelectorAll('nav>ul.pagination>li'), function(elm) {
-    elm.addEventListener('click', paginationInputItemSelect, false);
+  applyToElements(container.querySelectorAll('.page-item.value-item'), function(elm) {
+    elm.addEventListener('click', function() {paginationInputItemSelect(data, container, active);}, false);
+  });
+  applyToElements(container.querySelectorAll('.page-item:first-child'), function(elm) {
+    elm.addEventListener('click',function() {paginationInputPreviousPage(data, container, active);}, false);
+  });
+  applyToElements(container.querySelectorAll('.page-item:last-child'), function(elm) {
+    elm.addEventListener('click',function() {paginationInputNextPage(data, container, active);}, false);
   });
 }
 
