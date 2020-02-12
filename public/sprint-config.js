bf9359b33626b8e8f9f8637ce2ffcc2f4bbb7c57
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-12 11:54:32 -0300

    badge 19;

--- public/sprint-config.js
+++ public/sprint-config.js
@@ -1,5 +1,22 @@
 var t = TrelloPowerUp.iframe();
 
+var sprint = {
+  dias: 0,
+  inicio: moment()
+}
+
+t.render(function(){
+  return t.get('board', 'shared', 'sprint')
+  .then(function(sprintData){
+    sprint = sprintData || sprint;
+    elementById("totalDias").value = sprint.dias;
+    elementById("inicioSprint").value = sprint.inicio.format('DD/MM/YYYY');
+  })
+  .then(function(){
+    t.sizeTo('#springConfigForm').done();
+  });
+});
+
 $( function() {
   $("#inicioSprint").datepicker($.datepicker.regional[ "pt-BR" ]);
   $("#springConfigForm").on('submit', function(event){
@@ -9,10 +26,8 @@ $( function() {
     var dias = parseInt($("#totalDias").val()); 
     var mDate =  moment(dateTxt, "DD/MM/YYYY", true);
     if (mDate.isValid() && R.is(Number, dias) && !isNaN(dias)) {
-      var sprint = {
-        dias: dias,
-        inicio: mDate
-      };
+      sprint.dias = dias;
+      sprint.inicio = mDate;
       return t.set('board', 'shared', 'sprint', sprint)
           .then(function(){
             t.closePopup();
