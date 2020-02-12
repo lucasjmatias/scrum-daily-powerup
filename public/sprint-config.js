eadcbfdb101d297bfbe35074e4b2c0fbd6f79e1c
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-12 11:37:37 -0300

    badge 15;

--- public/sprint-config.js
+++ public/sprint-config.js
@@ -1,13 +1,20 @@
 $( function() {
-  $("#inicioSprint" ).datepicker($.datepicker.regional[ "pt-BR" ]);
-  window.springConfigForm.addEventListener('submit', function(event){
+  $("#inicioSprint").datepicker($.datepicker.regional[ "pt-BR" ]);
+  $("#springConfigForm").on('submit', function(event){
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
-    console.log($("#totalDias").val());
-    console.log($("#inicioSprint").val());
-    // return t.set('card', 'shared', 'pontuacao', pontuacao)
-    // .then(function(){
-    //   t.closePopup();
-    // });
+    var dateTxt = $("#inicioSprint").val(); 
+    var dias = parseInt($("#totalDias").val()); 
+    var mDate =  moment(dateTxt, "DD/MM/YYYY", true);
+    if (mDate.isValid() && R.is(Number, dias) && !isNaN(dias)) {
+      var sprint = {
+        dias: dias,
+        inicio: mDate
+      };
+      return t.set('board', 'shared', 'sprint', sprint)
+          .then(function(){
+            t.closePopup();
+          });
+    }
   });
 } );
