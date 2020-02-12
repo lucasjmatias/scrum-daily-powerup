8614aede384f577a7b9c1a09a3f910fa2f890dfd
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-12 10:01:10 -0300

    badge 4

--- public/client.js
+++ public/client.js
@@ -1,12 +1,9 @@
-var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
-var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
-
 var BADGE_ICON = './img/ballot-check-regular.svg';
 
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      icon: BADGE_ICON,
       text: 'Pontuar',
       callback: function(t) {
         return t.popup({
@@ -39,9 +36,9 @@ TrelloPowerUp.initialize({
         var total = parseInt(pontuacao.total);
         var done = contDone(pontuacao);
         return [{
-          title: 'Estimate',
-          text: pontuacao ?  done + ' / ' + total : null,
-          color: pontuacao ? null : 'red',
+          icon: BADGE_ICON,
+          text: total === done ? done : done + ' / ' + total,
+          color: total === done ? 'green' : done === 0 ? 'red' : 'yellow',
           callback: function(t) {
             return t.popup({
               title: "Pontuação",
