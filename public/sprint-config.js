2e58609ece32b9bfc8d276f0d0b31a34af4d54db
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-12 10:54:15 -0300

    badge 8

--- public/sprint-config.js
+++ public/sprint-config.js
@@ -1,3 +1,20 @@
 $( function() {
-  $( "#inicioSprint" ).datepicker();
+  $("#inicioSprint" ).datepicker();
+  $("#totalDias").on('keydown', function(event){
+      // doneInput.addEventListener('keydown', function(event) {
+    let key = Number(event.key)
+    if (event.which === 13) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+    if (event.which === 13 && doneInput.value) {
+      addDone(doneInput.value);
+    }
+    if (isNaN(key) || event.key===null) {
+      return false;
+    }
+    else {
+      console.log("is numeric")
+    }
+  }, false);
 } );
