95094c8910ecc50b3dad1edd5a40ea6511d7a35c
Lucas Matias <lucas.matias.87@gmail.com>
2020-02-14 10:37:43 -0300

    serviço calendário 6

--- src/controllers/FeriadoController.js
+++ src/controllers/FeriadoController.js
@@ -21,7 +21,7 @@ exports.get = (req, res, next) => {
       }
     })
     .then(response => {
-      res.status(201).json(JSON.parse(response));
+      res.status(201).send(response);
     })
     .catch(({message}) => res.status(502).json({message}));
 };
